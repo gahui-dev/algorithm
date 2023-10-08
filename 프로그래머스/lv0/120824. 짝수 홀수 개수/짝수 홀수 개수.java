fda7b2427a26070111b7bb16a3f01281756e79f4
이가희 <139962408+gahui-dev@users.noreply.github.com>
2023-10-08 19:12:30 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,13 +1,9 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = {0, 0};
+        int[] answer = new int[2];
         
-        for(int num : num_list) {
-            if(num % 2 == 0) {
-                answer[0]++;
-            } else {
-                answer[1]++;
-            }
+        for(int i = 0; i < num_list.length; i++) {
+            answer[num_list[i] % 2]++;
         }
 
         return answer;
