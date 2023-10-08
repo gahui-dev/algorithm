71eedddafc9be017fc772173615e0616842a0ae9
이가희 <139962408+gahui-dev@users.noreply.github.com>
2023-10-09 01:43:38 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,15 +1,8 @@
 class Solution {
     public int solution(int n) {
-        int answer = n / 7;
+        int answer = n % 7 == 0 ? n / 7 : n / 7 + 1;
         
-        if((n % 7) > 0 && (n % 7) < 7) {
-            answer++;
-            
-        }
-        
-        if(n < 7) {
-            answer = 1; 
-        }
         return answer;
+        
     }
 }
