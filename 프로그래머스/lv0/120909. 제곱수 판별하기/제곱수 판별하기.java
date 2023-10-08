573926eefa7f586e68d8fd395a7729576024dfd5
이가희 <139962408+gahui-dev@users.noreply.github.com>
2023-10-09 02:22:59 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            if((i * i) == n) {
+                return answer = 1;
+            }
+            answer = 2;
+        }
+        return answer;
+    }
+}
