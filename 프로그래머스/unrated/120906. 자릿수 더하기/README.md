0094bfb7558ebdd06b662dd013e4e275cfc43065
이가희 <139962408+gahui-dev@users.noreply.github.com>
2023-10-13 01:58:23 +0900

    [level unrated] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120906. 자릿수 더하기/README.md
+++ 프로그래머스/unrated/120906. 자릿수 더하기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 자릿수 더하기 - 120906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120906?language=java) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 0.03 ms
+메모리: 73.5 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 5일 1:53:27
+2023년 10월 5일 1:58:22
 
 ### 문제 설명
 
--- 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,11 +1,15 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        String s = Integer.toString(n);
+//         String s = Integer.toString(n);
 
-        for(int i = 0; i < s.length(); i++) {
-            // answer += Integer.parseInt(s.charAt(i));
-            answer += Integer.parseInt(s.substring(i, i + 1));
+//         for(int i = 0; i < s.length(); i++) {
+//             answer += Integer.parseInt(s.substring(i, i + 1));
+//         }
+        
+        while(n > 0) {
+            answer += n % 10;
+            n /= 10;
         }
         
         
