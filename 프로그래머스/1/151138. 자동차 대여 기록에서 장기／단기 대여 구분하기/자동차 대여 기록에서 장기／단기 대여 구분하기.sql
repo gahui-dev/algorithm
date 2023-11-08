22977199ab26363a3323830f9d8705c1ca48bfaa
이가희 <139962408+gahui-dev@users.noreply.github.com>
2023-11-08 16:13:04 +0900

    [level 1] Title: 자동차 대여 기록에서 장기/단기 대여 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
+++ 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
@@ -1,30 +1,20 @@
--- 코드를 입력하세요
-# SELECT HISTORY_ID
-#      , CAR_ID
-#      , DATE_FORMAT(START_DATE, '%Y-%m-%d') AS START_DATE
-#      , DATE_FORMAT(END_DATE, '%Y-%m-%d') AS END_DATE
-#      , "장기 대여" AS RENT_TYPE
-#   FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-#  WHERE DATEDIFF(END_DATE, START_DATE) >= 30
-#  UNION
-#  SELECT HISTORY_ID
-#      , CAR_ID
-#      , DATE_FORMAT(START_DATE, '%Y-%m-%d') AS START_DATE
-#      , DATE_FORMAT(END_DATE, '%Y-%m-%d') AS END_DATE
-#      , "단기 대여" AS RENT_TYPE
-#   FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-#  WHERE DATEDIFF(END_DATE, START_DATE) < 30
-#  ORDER BY HISTORY_ID DESC;
- 
- 
- SELECT HISTORY_ID
-      , CAR_ID
-      , DATE_FORMAT(START_DATE, '%Y-%m-%d') AS START_DATE
-      , DATE_FORMAT(END_DATE, '%Y-%m-%d') AS END_DATE
-      , CASE
-            WHEN DATEDIFF(END_DATE, START_DATE) + 1 >= 30
-            THEN '장기 대여'
-        ELSE '단기 대여' END AS RENT_TYPE
-  FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
- WHERE START_DATE BETWEEN '2022-09-01' AND '2022-09-30'
- ORDER BY HISTORY_ID DESC;
+ # SELECT HISTORY_ID
+ #      , CAR_ID
+ #      , DATE_FORMAT(START_DATE, '%Y-%m-%d') AS START_DATE
+ #      , DATE_FORMAT(END_DATE, '%Y-%m-%d') AS END_DATE
+ #      , CASE
+ #            WHEN DATEDIFF(END_DATE, START_DATE) + 1 >= 30
+ #            THEN '장기 대여'
+ #        ELSE '단기 대여' END AS RENT_TYPE
+ #  FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+ # WHERE START_DATE BETWEEN '2022-09-01' AND '2022-09-30'
+ # ORDER BY HISTORY_ID DESC;
+
+
+SELECT history_id, car_id
+, date_format(start_date, '%Y-%m-%d') as START_DATE
+, date_format(end_date, '%Y-%m-%d') as END_DATE
+, if(DATEDIFF(END_DATE, START_DATE) + 1 >= 30, '장기 대여', '단기 대여') as rent_type
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+where date_format(start_date, '%Y-%m') = '2022-09'
+order by history_id desc;
