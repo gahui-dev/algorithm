9248e3113aa59627add76e877e67aad4d9e5dcda
이가희 <139962408+gahui-dev@users.noreply.github.com>
2024-02-15 18:12:16 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,7 +1,8 @@
 function solution(a, b) {
-    var num1 = a + '' + b;
-    var num2 = b + '' + a;
+    var answer = Math.max(`${a}${b}`, `${b}${a}`)
+//     var num1 = a + '' + b;
+//     var num2 = b + '' + a;
     
-    var answer = num1 - num2 >= 0 ? num1 : num2
-    return Number(answer);
+//     var answer = num1 - num2 >= 0 ? num1 : num2
+    return answer;
 }
