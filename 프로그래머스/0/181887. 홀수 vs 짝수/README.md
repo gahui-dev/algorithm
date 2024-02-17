3f218921e372ca5d5dd8b9565131a2ff7dc22f5d
이가희 <139962408+gahui-dev@users.noreply.github.com>
2024-02-17 23:39:04 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/README.md
+++ 프로그래머스/0/181887. 홀수 vs 짝수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 홀수 vs 짝수 - 181887 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181887) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181887?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.09 ms
+메모리: 33.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 6일 23:35:38
+2024년 2월 6일 23:39:3
 
 ### 문제 설명
 
--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,16 +1,24 @@
 function solution(num_list) {
-    var answer = 0;
-    let odd = [];
-    let even = [];
-    
-    num_list.map((num, i) => {
-        i % 2 === 0 ? odd.push(num) : even.push(num); 
+    let even = 0;
+    let odd = 0
+
+    num_list.map((v, idx) => {
+        !(idx % 2) ? even += v : odd += v; 
     })
+
+    return odd > even ? odd : even; 
+    
+//     let odd = [];
+//     let even = [];
+    
+//     num_list.map((num, i) => {
+//         i % 2 === 0 ? odd.push(num) : even.push(num); 
+//     })
    
-    let oddSum = odd.reduce((a, v) => a + v);
-    let evenSum = even.reduce((a, v) => a+ v);
+//     let oddSum = odd.reduce((a, v) => a + v);
+//     let evenSum = even.reduce((a, v) => a+ v);
     
-    answer = oddSum > evenSum ? oddSum : evenSum;
+//     answer = oddSum > evenSum ? oddSum : evenSum;
     
-    return answer;
+//     return answer;
 }
